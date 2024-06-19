272d4d6a15422dd2ba23c625315fecae90c2f023
Mirolity <murachev.00@gmail.com>
2024-06-20 01:33:06 +0300

    Add commit function Add subtraction function Use add function in main.py Use subtracr function in main.py

--- py_project/main.py
+++ py_project/main.py
@@ -1,4 +1,10 @@
-from funcs.py import add
+from funcs import add
 
 result = add(2,3)
 print(f"Addition Result: {result}")
+
+from funcs import subtract
+
+result_sub = subtract(5,2)
+print(f"Subtraction Result: {result_sub}")
+
